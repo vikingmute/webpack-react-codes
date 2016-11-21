14540647c89f1565dec665da23e3327d760646b0
nighca <nighca@live.cn>
2016-11-21 09:58:51 +0800

    fix lint error

--- chapter5/part1/app/components/List.jsx
+++ chapter5/part1/app/components/List.jsx
@@ -5,14 +5,12 @@ const propType = {
   onDelete: PropTypes.func.isRequired
 };
 function List({ items, onDelete }) {
-  let itemList = items.map(item => {
-    return (
-      <li key={item.id}>
-        <button onClick={() => { onDelete(item.id); }}>删除</button>
-        {item.content}
-      </li>
-    );
-  });
+  let itemList = items.map(item => (
+    <li key={item.id}>
+      <button onClick={() => { onDelete(item.id); }}>删除</button>
+      {item.content}
+    </li>
+  ));
   return (
     <ul>
       <h2>这是今天的待办事项</h2>
